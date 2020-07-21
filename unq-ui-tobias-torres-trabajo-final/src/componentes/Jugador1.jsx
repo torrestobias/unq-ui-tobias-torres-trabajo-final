2b50e2f9eb8c8657766b6b32400cd53440591535
tobias torres <tobias.torres777@gmail.com>
2020-07-20 21:24:30 -0300

    se agregaron demas botones, no me esta funcionando bien el ganador, esta hardcodeado la eleccion de la computadora

--- unq-ui-tobias-torres-trabajo-final/src/componentes/Jugador1.jsx
+++ unq-ui-tobias-torres-trabajo-final/src/componentes/Jugador1.jsx
@@ -9,11 +9,23 @@ import Navbar from "./Navbar";
 
 function Jugador1 (){
 
-    const [seleccionJugador, setSeleccionJugador] = useState(null);  
+    const [seleccionJugador, setSeleccionJugador] = useState();  
 
-    const [seleccionComputadora, setSeleccionComputadora] = useState(null);
+    const [seleccionComputadora, setSeleccionComputadora] = useState();
+
+    const [eleccionJugadorString, setEleccionJugadorString] = useState();
+
+    const [eleccionComputadoraString, setEleccionComputadoraString] = useState("");
+
+
+    const [sePuedeJugar, setSePuedeJugar] = useState(true);
+
+    const [ganador, setGanador] = useState("");
+
 
     const eleccionesPosiblesDeComputadora = [Piedra, Papel, Tijera, Lagarto, Spock];
+
+    
 
     const numeroAleatorio = [1,2,3,4,5];
 
@@ -21,32 +33,64 @@ function Jugador1 (){
 
     var eleccion = eleccionesPosiblesDeComputadora[randomNumber];
 
-    function funcionRica(){
-        setSeleccionJugador(null)
-        setSeleccionComputadora(null)
+    function reinicioElecciones(){
+        setSeleccionJugador(null);
+        setSeleccionComputadora(null);
+        setSePuedeJugar(true);
+        setGanador(null);
     }
 
-    
+    function inicioDeJuegoRico(){
+        if(sePuedeJugar){
+            setSeleccionComputadora(<DisplayImage pathImage={Tijera}/>);
+
+            setEleccionComputadoraString("Tijera");
+
+            console.log("hi", eleccionComputadoraString);
+            ganadorDePartidaRica();
+
+            setSePuedeJugar(false);
+        }
+    }
+
+    function ganadorDePartidaRica(){
+        if(eleccionJugadorString === "Piedra"){
+           if(eleccionComputadoraString === "Tijera"){
+            setGanador("Ganaste");
+           }
+        }
+    }
+
+    function conversor(piedraImagen, piedraString){
+        setSeleccionJugador(piedraImagen)
+        setEleccionJugadorString(piedraString)
+    }
 
     return (
         <div className="divBar">
-            <Navbar />
             <div className="botones">
-                <div className="row">   
+                <div className="row">  
                     <h3>Elegiste a: {seleccionJugador}</h3>
                 </div>
                 <div className="row">
-                    <a class="waves-effect waves-light btn-small" onClick={() => {setSeleccionJugador(<DisplayImage pathImage={Piedra}/>)}}>Piedra</a>
-                    <a class="waves-effect waves-light btn-small" onClick={() => {setSeleccionJugador(<DisplayImage pathImage={Papel}/>)}}>Papel</a>
-                    <a class="waves-effect waves-light btn-small" onClick={() => {setSeleccionJugador(<DisplayImage pathImage={Tijera}/>)}}>Tijera</a>
-                    <a class="waves-effect waves-light btn-small" onClick={() => {setSeleccionJugador(<DisplayImage pathImage={Lagarto}/>)} }>Lagarto</a>
-                    <a class="waves-effect waves-light btn-small" onClick={() => {setSeleccionJugador(<DisplayImage pathImage={Spock}/>)}}>Spock</a>
+                    <a className="waves-effect waves-light btn-small" onClick={() => {conversor(<DisplayImage pathImage={Piedra}/>,"Piedra")}}>Piedra</a>
+                    <a className="waves-effect waves-light btn-small" onClick={() => {setSeleccionJugador(<DisplayImage pathImage={Papel}/>)}}>Papel</a>
+                    <a className="waves-effect waves-light btn-small" onClick={() => {setSeleccionJugador(<DisplayImage pathImage={Tijera}/>)}}>Tijera</a>
+                    <a className="waves-effect waves-light btn-small" onClick={() => {setSeleccionJugador(<DisplayImage pathImage={Lagarto}/>)} }>Lagarto</a>
+                    <a className="waves-effect waves-light btn-small" onClick={() => {setSeleccionJugador(<DisplayImage pathImage={Spock}/>)}}>Spock</a>
                 </div>
-                <div className="row">   
+                <div className="row">
                     <h4>La computadora eligio: {seleccionComputadora} </h4>
-                    <a class="waves-effect waves-light btn-small" onClick={() => {setSeleccionComputadora(<DisplayImage pathImage={eleccion}/>)}}>Jugar</a>
+                    <a className="waves-effect waves-light btn-small" onClick={() => {inicioDeJuegoRico()}}>Jugar</a>
                 </div>
-                <a class="waves-effect waves-light btn-small" onClick={() => {funcionRica()}}>Volver a Jugar</a>
+                <div className="row"> 
+                    <h4>El Resultado es: {ganador}</h4>
+                </div>
+                <div className="row"> 
+                    <a className="waves-effect waves-light btn-small" onClick={() => {reinicioElecciones()}}>Volver a Jugar</a>
+                </div>
+
+
             </div>
         </div>
     )
